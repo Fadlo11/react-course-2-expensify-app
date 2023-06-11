077e5245188a739e94406ce03a5973429227bc6d
Fadlo <fadlallah.chamata@gmail.com>
2023-06-11 10:19:38 +0300

    remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -18,18 +18,18 @@ const store = configureStore();
 //     console.log(visibleExpenses);
 // });
 
- store.dispatch(addExpense({ description: 'Water Bill', amount: 150 }));
- store.dispatch(addExpense({ description: 'Gas Bill', createdAt:1000 }));
- store.dispatch(addExpense({ description: 'Rent', amount: 2500 }));
+//  store.dispatch(addExpense({ description: 'Water Bill', amount: 150 }));
+//  store.dispatch(addExpense({ description: 'Gas Bill', createdAt:1000 }));
+//  store.dispatch(addExpense({ description: 'Rent', amount: 2500 }));
 
 // store.dispatch(setTextFilter('Water'));
 // setTimeout(() =>{
 //     store.dispatch(setTextFilter('bill'));
 // },3000)
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpenses);
 // console.log(store.getState());
 
 const jsx = (
